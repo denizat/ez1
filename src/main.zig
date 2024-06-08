31aba2375ab18fcf4b2549652506b627461ab6ef
Deniz Telci <deniza.telci@gmail.com>
2024-06-08 14:58:10 -0500

    clean up and fix some bugs

--- src/main.zig
+++ src/main.zig
@@ -7,28 +7,12 @@ pub fn main() void {
     var arl = std.ArrayList(u8).init(al);
     var namebuf: [std.posix.HOST_NAME_MAX]u8 = undefined;
 
+    const user = std.posix.getenv("USER") orelse "???";
+    arl.appendSlice(user) catch unreachable;
+    arl.append('@') catch unreachable;
     const hn = std.posix.gethostname(&namebuf) catch unreachable;
     arl.appendSlice(hn) catch unreachable;
-    const user = std.posix.getenv("USER") orelse "???";
-    arl.append('@') catch unreachable;
-    arl.appendSlice(user) catch unreachable;
     arl.append(' ') catch unreachable;
-    var outb: [256]u8 = undefined;
-    const o = std.fs.cwd().realpath(".", &outb) catch unreachable;
-    arl.appendSlice(o) catch unreachable;
-    const status = std.posix.getenv("STATUS") orelse "";
-    if (status.len > 0) {
-        arl.append(' ') catch unreachable;
-        arl.appendSlice(status) catch unreachable;
-    }
-
-    // const child = std.process.Child.run(.{
-    //     .allocator = al,
-    //     .argv = &.{ "git", "branch", "--show-current" },
-    // }) catch unreachable;
-    // arl.appendSlice(" (") catch unreachable;
-    // arl.appendSlice(child.stdout[0 .. child.stdout.len - 1]) catch unreachable;
-    // arl.append(')') catch unreachable;
 
     const git = getGit(al);
     if (git) |g| {
@@ -37,15 +21,37 @@ pub fn main() void {
         arl.append(')') catch unreachable;
     }
 
+    const status = std.posix.getenv("STATUS") orelse "";
+    if (status.len > 0) {
+        arl.append(' ') catch unreachable;
+        arl.appendSlice(status) catch unreachable;
+    }
+
+    arl.append('\n') catch unreachable;
+
+    var outb: [256]u8 = undefined;
+    const o = std.fs.cwd().realpath(".", &outb) catch unreachable;
+    const spath = shortenPath(o);
+    arl.appendSlice(spath) catch unreachable;
+
     arl.appendSlice("\n> ") catch unreachable;
     const out = std.io.getStdOut().writer();
     out.writeAll(arl.items) catch unreachable;
 }
 
+// WE OWN PATH NOW!!!!
+fn shortenPath(path: []u8) []const u8 {
+    const home = std.posix.getenv("HOME") orelse return path;
+    const res = std.mem.startsWith(u8, path, home);
+    if (res) {
+        path[home.len - 1] = '~';
+        return path[home.len - 1 ..];
+    }
+    return path;
+}
+
 fn getGit(al: std.mem.Allocator) ?[]const u8 {
-    const cwdp = std.fs.cwd().realpathAlloc(al, ".") catch {
-        return null;
-    };
+    const cwdp = std.fs.cwd().realpathAlloc(al, ".") catch return null;
     const dir = findDirUpwards(al, cwdp, ".git") orelse {
         return null;
     };
@@ -60,23 +66,6 @@ fn getGit(al: std.mem.Allocator) ?[]const u8 {
     }
     return last[0 .. last.len - 1];
 }
-
-// fn findDirUpwards(startDir: std.fs.Dir, name: []const u8) ?std.fs.Dir {
-//     // REMOVE THIS
-//     const al = std.heap.page_allocator;
-//     const cwd = std.fs.cwd();
-//     defer cwd.close();
-//     const d = cwd.openDir(name, .{}) catch {
-//         const stat = cwd.stat() catch unreachable ;
-//         // we are at root and there will be no .git here
-//         if (stat.inode == 2) {
-//             return null;
-//         }
-//         const path = cwd.realpathAlloc(al, "..") catch unreachable;
-//         return findDirUpwards(startDir: std.fs.Dir, name: []const u8)
-//     };
-//     return d;
-// }
 
 test "mini test" {
     const al = std.testing.allocator;
@@ -97,14 +86,14 @@ test "mini test" {
     std.debug.print("{any}\n", .{res});
 }
 fn findDirUpwards(al: std.mem.Allocator, startPath: []const u8, name: []const u8) ?std.fs.Dir {
-    const dir = std.fs.openDirAbsolute(startPath, .{}) catch {
-        return null;
-    };
+    const dir = std.fs.openDirAbsolute(startPath, .{}) catch return null;
+    const stat = dir.stat() catch return null;
     const target = dir.openDir(name, .{});
     if (target) |t| return t else |_| {}
-    const path = dir.realpathAlloc(al, "..") catch {
+    if (stat.inode == 2) {
         return null;
-    };
+    }
+    const path = dir.realpathAlloc(al, "..") catch return null;
     defer al.free(path);
     return findDirUpwards(al, path, name);
 }
